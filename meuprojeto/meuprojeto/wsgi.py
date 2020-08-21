f0b722b0ecb3258a4af0e48624106b9cf664eca6
Lucas Carrias <lucascarrias@outlook.com>
2020-08-21 14:03:27 -0300

    Criar novo projeto na branch

--- meuprojeto/meuprojeto/wsgi.py
+++ meuprojeto/meuprojeto/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for connectedin project.
+WSGI config for meuprojeto project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'connectedin.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'meuprojeto.settings')
 
 application = get_wsgi_application()
