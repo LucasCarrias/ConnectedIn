1f83574cd56c5d571d758184a1118785e699c560
Lucas Carrias <lucascarrias@outlook.com>
2020-09-25 22:01:56 -0300

    Create models. Add models to admin

--- connectedin/core/admin.py
+++ connectedin/core/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import UserAccount, Profile, Post, Comment, Reaction, PostReaction
 
-# Register your models here.
+
+@admin.register(UserAccount)
+class UserAccountAdmin(admin.ModelAdmin):
+    exclude = ('password', 'groups', 'user_permissions', )
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    filter_horizontal = ('contacts',)
+    list_display = ('name', 'user',)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    fields = ('profile', 'body', 'created', 'updated')
+    readonly_fields = ('created', 'updated',)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Reaction)
+class ReactionAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(PostReaction)
+class PostReactionAdmin(admin.ModelAdmin):
+    pass
--- connectedin/core/models.py
+++ connectedin/core/models.py
@@ -1,3 +1,74 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserAccount(User):
+    birth_date = models.DateField(blank=True, null=True)
+
+
+class Profile(models.Model):
+    user = models.ForeignKey(UserAccount, on_delete=models.CASCADE, related_name='profiles')
+    contacts = models.ManyToManyField('self', blank=True)
+    name = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Invitation(models.Model):
+    STATUS_CHOICES = [
+        ('accepted', 'Accepted'),
+        ('rejected', 'Rejected'),
+        ('waiting', 'Waiting'),
+    ]
+
+    user_from = models.ForeignKey(Profile,
+                                  related_name='invite_from',
+                                  on_delete=models.CASCADE)
+    user_to = models.ForeignKey(Profile,
+                                related_name='invite_to',
+                                on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(choices=STATUS_CHOICES, default='accepted')
+
+    class Meta:
+        ordering = ('-created')
+
+class Post(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="posts")
+    body = models.TextField(null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.profile.name}'s post from {self.created}"
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.profile.name}'s comment in {self.post.profile.name}'s post"
+
+
+class Reaction(models.Model):
+    name = models.CharField(max_length=10)
+    weigth = models.SmallIntegerField()
+
+    def __str__(self):
+        return self.name
+
+class PostReaction(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    reaction = models.ForeignKey(Reaction, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.profile.name}'s reaction in {self.post.profile.name}'s post"
